dce776040ba1102b9fdd1a7903d28bd0b228d07a
Taewoongjung <aipooh8882@naver.com>
2022-09-20 15:36:54 +0900

    first commit

---
+++ src/main/java/app/foodylab/FoodyLabApplication.java
@@ -0,0 +1,13 @@
+package app.foodylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodyLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodyLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/foodylab/FoodyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package app.foodylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodyLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
