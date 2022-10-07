a6aa6c65e5cae0886c87c279fd420c5a0be5559a
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-07 11:19:33 +0900

    쿠폰, 쿠폰 테스트 리팩토링
    assertThrows의 message는 익셉션의 메세지를 테스트하는게 아님
    이를 보완하기 위해 assertj 사용
    predicate를 이용하여 조건절을 읽기 편하게 수정

--- src/main/java/app/foodylab/coupon/domain/PriceCoupon.java
+++ src/main/java/app/foodylab/coupon/domain/PriceCoupon.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 @RequiredArgsConstructor
 @Getter
@@ -13,33 +15,25 @@ public class PriceCoupon {
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public static PriceCoupon of(int saledPrice) {
-        isNegative(saledPrice);
+    public static PriceCoupon of(int discountPrice) {
+        require(i -> i <= 0, discountPrice, "할인 금액은 0보다 커야 합니다.");
         LocalDate now = LocalDate.now();
-        return of(saledPrice, now, now.plusYears(1));
+        return of(discountPrice, now, now.plusYears(1));
     }
 
-    public static PriceCoupon of(int saledPrice, LocalDate startDate, LocalDate endDate) {
-        isNegative(saledPrice);
-        isValidStartDate(startDate);
-        isValidEndDate(endDate);
-        isValidDate(startDate, endDate);
-        return new PriceCoupon(saledPrice, startDate, endDate);
+    public static PriceCoupon of(int discountPrice, LocalDate startDate, LocalDate endDate) {
+        require(i -> i <= 0, discountPrice, "할인 금액은 0보다 커야 합니다.");
+        require(sDate -> sDate.isBefore(LocalDate.now()), startDate, "쿠폰 사용 시작일은 현재보다 이전일 수 없습니다.");
+        require(eDate -> eDate.isBefore(LocalDate.now()), endDate, "쿠폰 사용 종료일은 현재보다 이전일 수 없습니다.");
+        require((sDate, eDate) -> eDate.isBefore(sDate), startDate, endDate, "쿠폰 사용 종료일은 시작일 이전일 수 없습니다.");
+        return new PriceCoupon(discountPrice, startDate, endDate);
     }
 
-    private static void isValidDate(LocalDate startDate, LocalDate endDate) {
-        if (endDate.isBefore(startDate)) throw new IllegalArgumentException("쿠폰 사용 종료일은 시작일 이전일 수 없습니다.");
+    private static <T> void require(Predicate<T> predicate, T target, String msg) {
+        if (predicate.test(target)) throw new IllegalArgumentException(msg);
     }
 
-    private static void isValidEndDate(LocalDate endDate) {
-        if (endDate.isBefore(LocalDate.now())) throw new IllegalArgumentException("쿠폰 사용 종료일은 현재보다 이전일 수 없습니다.");
-    }
-
-    private static void isValidStartDate(LocalDate startDate) {
-        if (startDate.isBefore(LocalDate.now())) throw new IllegalArgumentException("쿠폰 사용 시작일은 현재보다 이전일 수 없습니다.");
-    }
-
-    private static void isNegative(int saledPrice) {
-        if (saledPrice <= 0) throw new IllegalArgumentException("할인 금액은 0보다 커야 합니다.");
+    private static <T, V> void require(BiPredicate<T, V> predicate, T target1, V target2, String msg) {
+        if (predicate.test(target1, target2)) throw new IllegalArgumentException(msg);
     }
 }
--- src/test/java/app/foodylab/coupon/domain/PriceCouponTest.java
+++ src/test/java/app/foodylab/coupon/domain/PriceCouponTest.java
@@ -6,35 +6,39 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class PriceCouponTest {
 
     @Test
     @DisplayName("할인 쿠폰의 할인 금액이 음수이면 익셉션 발생해야 한다.")
     void test1() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(-10000), "할인 금액은 0보다 커야 합니다.");
+        assertThat(assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(-10000))).hasMessageContaining("할인 금액은 0보다 커야 합니다.");
     }
 
     @Test
     @DisplayName("할인 쿠폰의 할인 금액이 양수이면 정상적으로 생성해야 한다.")
     void test2() {
-        Assertions.assertDoesNotThrow(() -> PriceCoupon.of(10000));
+        assertDoesNotThrow(() -> PriceCoupon.of(10000));
     }
 
     @Test
     @DisplayName("사용 시작일이 현재보다 이전일 경우 생성 불가해야 한다.")
     void test3() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(10000, LocalDate.now().minusDays(1), LocalDate.now().plusYears(1)), "쿠폰 사용 시작일은 현재보다 이전일 수 없습니다.");
+        assertThat(assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(10000, LocalDate.now().minusDays(1), LocalDate.now().plusYears(1)))).hasMessageContaining("쿠폰 사용 시작일은 현재보다 이전일 수 없습니다.");
     }
 
     @Test
     @DisplayName("사용 종료일이 현재보다 이전일 경우 생성 불가해야 한다.")
     void test4() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(10000, LocalDate.now(), LocalDate.now().minusDays(1)), "쿠폰 사용 종료일은 현재보다 이전일 수 없습니다.");
+        assertThat(assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(10000, LocalDate.now(), LocalDate.now().minusDays(1)))).hasMessageContaining("쿠폰 사용 종료일은 현재보다 이전일 수 없습니다.");
     }
 
     @Test
     @DisplayName(" [사용 시작일 > 사용 종료일]이면 생성 불가해야 한다.")
     void test5() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(10000, LocalDate.now().plusDays(2), LocalDate.now().plusDays(1)), "쿠폰 사용 종료일은 시작일 이전일 수 없습니다.");
+        assertThat(assertThrows(IllegalArgumentException.class, () -> PriceCoupon.of(10000, LocalDate.now().plusDays(2), LocalDate.now().plusDays(1)))).hasMessageContaining("쿠폰 사용 종료일은 시작일 이전일 수 없습니다.");
     }
 }
