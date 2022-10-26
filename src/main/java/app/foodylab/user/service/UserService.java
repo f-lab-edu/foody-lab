d4d8e32df56827da70d906cc004368ad7d5756c9
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-26 12:21:43 +0900

    롬복을 이용해 생성자 주입 구현

--- src/main/java/app/foodylab/user/service/UserService.java
+++ src/main/java/app/foodylab/user/service/UserService.java
@@ -2,17 +2,16 @@ package app.foodylab.user.service;
 
 import app.foodylab.user.application.repository.UserRepository;
 import app.foodylab.user.domain.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserService userService;
 
     public void saveUser(User user) {
         userRepository.insert(user);
