643041946a455ef3bb025d1ba0cc5e43c628ef9d
Taewoongjung <aipooh8882@naver.com>
2022-10-28 14:56:22 +0900

    UserDTO 지움

--- src/testFixtures/java/app/foodylab/UserFixture.java
+++ src/testFixtures/java/app/foodylab/UserFixture.java
@@ -1,16 +1,11 @@
 package app.foodylab;
 
-import app.foodylab.application.user.dto.UserDTO;
 import app.foodylab.domain.user.User;
 
 public class UserFixture {
 
     public static final User USER = new User(
         1,
-        "heung min son"
-    );
-
-    public static final UserDTO USER_DTO = new UserDTO(
         "heung min son"
     );
 }
