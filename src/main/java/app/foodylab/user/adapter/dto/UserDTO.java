0a05ed47ed8813259f274702cc0b208758e6cd0f
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-28 10:13:33 +0900

    mapstruct를 이용하여 dto-vo 매핑

---
+++ src/main/java/app/foodylab/user/adapter/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package app.foodylab.user.adapter.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDTO {
+    private final String id;
+    private final String name;
+}
