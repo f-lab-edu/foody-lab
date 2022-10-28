961773ca28cee8a01a1fbd6e78e84abcfc7448f8
Taewoongjung <aipooh8882@naver.com>
2022-10-28 14:43:37 +0900

    테스트 복구 후 커밋

--- src/test/java/app/foodylab/user/service/UserServiceTest.java
+++ src/test/java/app/foodylab/user/service/UserServiceTest.java
@@ -1,29 +1,25 @@
 package app.foodylab.user.service;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import app.foodylab.UserFixture;
 import app.foodylab.adaptor.mapper.UserMapper;
 import app.foodylab.application.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UserServiceTest {
 
-//    private final UserMapper userRepository = mock(UserMapper.class);
-//    private final UserService userService = new UserService(userRepository);
-
-    @Autowired
-    private UserMapper userRepository;
+    private final UserMapper userRepository = mock(UserMapper.class);
+    private final UserService userService = new UserService(userRepository);
 
     @Test
     @DisplayName("유저 저장 테스트")
     void test1() {
-        UserService userService = new UserService(userRepository);
         userService.saveUser(UserFixture.USER);
-//        userService.saveUser(UserFixture.USER);
-//        verify(userRepository, times(1)).userInsert(UserFixture.USER);
+        verify(userRepository, times(1)).userInsert(UserFixture.USER);
     }
 
 }
