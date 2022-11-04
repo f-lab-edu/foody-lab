9a81b671a8cce3f0c15eeca51fa736ba4de3bc88
Taewoongjung <aipooh8882@naver.com>
2022-11-04 22:00:07 +0900

    TestInstance 어노테이션 테스트코드에 추가

--- src/test/java/app/foodylab/application/order/service/OrderServiceTest.java
+++ src/test/java/app/foodylab/application/order/service/OrderServiceTest.java
@@ -13,7 +13,10 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class OrderServiceTest {
 
     RateDiscountPolicySpy rateDiscountPolicy = new RateDiscountPolicySpy();
