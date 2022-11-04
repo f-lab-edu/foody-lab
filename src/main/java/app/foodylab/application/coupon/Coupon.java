b63cf9313824bac8ad665c7d09308fe92763e0ce
Taewoongjung <aipooh8882@naver.com>
2022-11-04 21:49:05 +0900

    Coupon 인터페이스에 메소드 정의 추가

--- src/main/java/app/foodylab/application/coupon/Coupon.java
+++ src/main/java/app/foodylab/application/coupon/Coupon.java
@@ -1,6 +1,10 @@
 package app.foodylab.application.coupon;
 
+import app.foodylab.domain.coupon.CouponType;
+
 public interface Coupon {
 
     long getDiscountPrice();
+
+    CouponType getCouponType();
 }
