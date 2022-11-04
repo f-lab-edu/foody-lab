1685240a54f51f49ef5f3597258c846de5d8ff2e
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-04 17:37:06 +0900

    명확한 이름으로 변경

--- src/test/java/app/foodylab/application/payment/service/PayServiceTest.java
+++ src/test/java/app/foodylab/application/payment/service/PayServiceTest.java
@@ -1,7 +1,6 @@
 package app.foodylab.application.payment.service;
 
 import app.foodylab.OrderDTOFixture;
-import app.foodylab.OrderFixture;
 import app.foodylab.payment.KakaoPaySpy;
 import java.util.Collections;
 import java.util.List;
@@ -23,7 +22,7 @@ class PayServiceTest {
     @Test
     @DisplayName("페이 서비스를 호출하면 pay 메소드가 호출되는지 테스트")
     void test1() {
-        payService.pay(OrderDTOFixture.ORDERDTO, "kakaoPay", Collections.EMPTY_LIST);
+        payService.pay(OrderDTOFixture.DUMMY, "kakaoPay", Collections.EMPTY_LIST);
         Assertions.assertTrue(kakaoPay.isPaid());
     }
 }
--- src/testFixtures/java/app/foodylab/OrderDTOFixture.java
+++ src/testFixtures/java/app/foodylab/OrderDTOFixture.java
@@ -4,5 +4,5 @@ import app.foodylab.adaptor.OrderDto;
 import java.time.LocalDate;
 
 public class OrderDTOFixture {
-    public static OrderDto ORDERDTO = new OrderDto(10L, "1", "1", 15000L, LocalDate.now());
+    public static OrderDto DUMMY = new OrderDto(10L, "1", "1", 15000L, LocalDate.now());
 }
