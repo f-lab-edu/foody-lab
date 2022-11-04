b0f67d87efeedaa89d09966cde2fe73e63c6f536
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-04 15:08:32 +0900

    가격이 아닌 주문 클래스를 받아 처리하도록 수정

--- src/main/java/app/foodylab/adaptor/payment/ExternalKakaoPayImpl.java
+++ src/main/java/app/foodylab/adaptor/payment/ExternalKakaoPayImpl.java
@@ -1,24 +1,33 @@
 package app.foodylab.adaptor.payment;
 
 import app.foodylab.application.payment.ExternalPayAPI;
+import app.foodylab.domain.order.Order;
+import java.time.LocalDate;
 
 public class ExternalKakaoPayImpl implements ExternalPayAPI {
 
     @Override
-    public boolean processPay(long price) {
-        return requestPay() && approvePay();
+    public boolean processPay(Order order) {
+        KakaoPayResp kakaoPayResp = approvePay(order);
+        String responseCode = kakaoPayResp.getResponseCode();
+        return requestPay() && isApproved(responseCode);
+    }
+
+    private boolean isApproved(String responseCode) {
+        return "200".equals(responseCode);
     }
 
     private boolean requestPay() {
         return true;
     }
 
-    private boolean approvePay() {
+    private KakaoPayResp approvePay(Order order) {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return true;
+        LocalDate now = LocalDate.now();
+        return new KakaoPayResp("200", order.getOrderPrice(), "id", now, now);
     }
 }
