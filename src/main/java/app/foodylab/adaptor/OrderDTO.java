1d276fbf8a445af3d1466643f3981f03ae4d6c03
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-04 15:33:34 +0900

    orderDTO 추가

---
+++ src/main/java/app/foodylab/adaptor/OrderDTO.java
@@ -0,0 +1,15 @@
+package app.foodylab.adaptor;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class OrderDTO {
+    private final long id;
+    private final String customerId;
+    private final String storeId;
+    private final long orderPrice;
+    private final LocalDate orderedDate;
+}
