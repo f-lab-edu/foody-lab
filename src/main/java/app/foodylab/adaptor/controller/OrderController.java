63e1c88857fdbe82d89a9f1ce4849ef007a02a69
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-06 12:47:27 +0900

    오타 수정

--- src/main/java/app/foodylab/adaptor/controller/OrderController.java
+++ src/main/java/app/foodylab/adaptor/controller/OrderController.java
@@ -1,17 +1,14 @@
 package app.foodylab.adaptor.controller;
 
-import app.foodylab.adaptor.OrderDto;
 import app.foodylab.adaptor.OrderRequest;
-import app.foodylab.application.coupon.Coupon;
 import app.foodylab.application.payment.service.PayService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class Ordercontroller {
+public class OrderController {
 
     private final PayService payService;
 
