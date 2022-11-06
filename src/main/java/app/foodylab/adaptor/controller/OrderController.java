2a2b7f4f31898d74e297d02e61525287099ef6d0
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-06 15:04:08 +0900

    @PostMapping 추가

--- src/main/java/app/foodylab/adaptor/controller/OrderController.java
+++ src/main/java/app/foodylab/adaptor/controller/OrderController.java
@@ -3,6 +3,7 @@ package app.foodylab.adaptor.controller;
 import app.foodylab.adaptor.OrderRequest;
 import app.foodylab.application.payment.service.PayService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class OrderController {
 
     private final PayService payService;
 
+    @PostMapping("/pay")
     public void requestOrder(@RequestBody OrderRequest orderRequest) {
         payService.pay(orderRequest.getOrderDto(), orderRequest.getPayMethod(), orderRequest.getCoupons());
     }
