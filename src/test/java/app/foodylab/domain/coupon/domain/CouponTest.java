e15b7878cc3fa8fc692f16fc67616702ebb6916c
Taewoongjung <aipooh8882@naver.com>
2022-11-07 00:05:12 +0900

    CouponTest has been changed

--- src/test/java/app/foodylab/domain/coupon/domain/CouponTest.java
+++ src/test/java/app/foodylab/domain/coupon/domain/CouponTest.java
@@ -9,27 +9,27 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class PriceCouponTest {
+class CouponTest {
 
     @Test
     @DisplayName("할인 쿠폰의 할인 금액이 음수이면 익셉션 발생해야 한다.")
     void test1() {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-            () -> Coupon.of(-10000L));
+            () -> Coupon.of(-10000L, "할인쿠폰", "가격할인쿠폰"));
         assertThat(e.getMessage()).isEqualTo("할인 금액은 0보다 커야 합니다.");
     }
 
     @Test
     @DisplayName("할인 쿠폰의 할인 금액이 양수이면 정상적으로 생성해야 한다.")
     void test2() {
-        assertDoesNotThrow(() -> Coupon.of(10000L));
+        assertDoesNotThrow(() -> Coupon.of(10000L, "할인쿠폰", "가격할인쿠폰"));
     }
 
     @Test
     @DisplayName("사용 시작일이 현재보다 이전일 경우 생성 불가해야 한다.")
     void test3() {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-            () -> Coupon.of(10000L, LocalDate.now().minusDays(1),
+            () -> Coupon.of(10000L, "할인쿠폰", "가격할인쿠폰", LocalDate.now().minusDays(1),
                 LocalDate.now().plusYears(1)));
         assertThat(e.getMessage()).isEqualTo("쿠폰 사용 시작일은 현재보다 이전일 수 없습니다.");
 
@@ -39,7 +39,8 @@ class PriceCouponTest {
     @DisplayName("사용 종료일이 현재보다 이전일 경우 생성 불가해야 한다.")
     void test4() {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-            () -> Coupon.of(10000L, LocalDate.now(), LocalDate.now().minusDays(1)));
+            () -> Coupon.of(10000L, "할인쿠폰", "가격할인쿠폰", LocalDate.now(),
+                LocalDate.now().minusDays(1)));
         assertThat(e.getMessage()).isEqualTo("쿠폰 사용 종료일은 현재보다 이전일 수 없습니다.");
     }
 
@@ -47,7 +48,8 @@ class PriceCouponTest {
     @DisplayName(" [사용 시작일 > 사용 종료일]이면 생성 불가해야 한다.")
     void test5() {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-            () -> Coupon.of(10000L, LocalDate.now().plusDays(2), LocalDate.now().plusDays(1)));
+            () -> Coupon.of(10000L, "할인쿠폰", "가격할인쿠폰", LocalDate.now().plusDays(2),
+                LocalDate.now().plusDays(1)));
         assertThat(e.getMessage()).isEqualTo("쿠폰 사용 종료일은 시작일 이전일 수 없습니다.");
     }
 }
