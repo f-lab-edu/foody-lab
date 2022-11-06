66cf7f26df213bce514029b0803c1225d472864e
Taewoongjung <aipooh8882@naver.com>
2022-11-07 00:08:04 +0900

    CouponType has changed

--- src/main/java/app/foodylab/domain/coupon/CouponType.java
+++ src/main/java/app/foodylab/domain/coupon/CouponType.java
@@ -1,5 +1,18 @@
 package app.foodylab.domain.coupon;
 
+import app.foodylab.domain.discount.DiscountPolicy;
+import app.foodylab.domain.discount.PriceDiscountPolicy;
+import app.foodylab.domain.discount.RateDiscountPolicy;
+import lombok.Getter;
+
 public enum CouponType {
-    PRICE, RATE
+    PRICE(new PriceDiscountPolicy()),
+    RATE(new RateDiscountPolicy());
+
+    @Getter
+    private final DiscountPolicy policy;
+
+    CouponType(DiscountPolicy policy) {
+        this.policy = policy;
+    }
 }
