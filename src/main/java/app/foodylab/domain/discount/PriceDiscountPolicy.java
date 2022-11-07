5379627cca04d2e8b5da15267a54d01170655c72
Taewoongjung <aipooh8882@naver.com>
2022-11-07 17:34:14 +0900

    할인 정책 변수명 수정

--- src/main/java/app/foodylab/domain/discount/PriceDiscountPolicy.java
+++ src/main/java/app/foodylab/domain/discount/PriceDiscountPolicy.java
@@ -3,8 +3,8 @@ package app.foodylab.domain.discount;
 public class PriceDiscountPolicy implements DiscountPolicy {
 
     @Override
-    public long discount(long productPrice, long discountAmount) {
-        return productPrice - discountAmount;
+    public long discount(long productPrice, long discountPrice) {
+        return productPrice - discountPrice;
     }
 
 }
--- src/main/java/app/foodylab/domain/discount/RateDiscountPolicy.java
+++ src/main/java/app/foodylab/domain/discount/RateDiscountPolicy.java
@@ -3,8 +3,8 @@ package app.foodylab.domain.discount;
 public class RateDiscountPolicy implements DiscountPolicy {
 
     @Override
-    public long discount(long productPrice, long discountAmount) {
-        return productPrice * discountAmount / 100;
+    public long discount(long productPrice, long discountRate) {
+        return productPrice * discountRate / 100;
     }
 
 }
